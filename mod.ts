58b2d182e89080f99c1e99970d43f2557448101f
kimizuy <kimizuyamasaki@gmail.com>
2024-06-21 18:57:16 -0700

    initial commit

---
+++ mod.ts
@@ -0,0 +1,15 @@
+// mod.ts
+/**
+ * A module providing a function to greet people.
+ *
+ * @example
+ * ```ts
+ * import { greet } from "@kimizuy/greet";
+ *
+ * greet("kimizuy");
+ * ```
+ *
+ * @module
+ */
+
+export * from "./greet.ts";
